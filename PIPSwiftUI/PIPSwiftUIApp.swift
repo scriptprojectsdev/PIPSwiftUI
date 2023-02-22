592e4fc586c8ecfcf700ac6d29c21bd1e36eeb72
Erick Del Orbe <erick@scriptprojects.com>
2023-02-21 19:32:15 -0600

    Initial Commit

---
+++ PIPSwiftUI/PIPSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  PIPSwiftUIApp.swift
+//  PIPSwiftUI
+//
+//  Created by Erick Del Orbe on 2/21/23.
+//
+
+import SwiftUI
+
+@main
+struct PIPSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
